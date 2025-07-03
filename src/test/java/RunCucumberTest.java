82490426071728309d795e78191a13eee182e53b
Winfred <winfred@easyuse.com.tw>
2025-07-03 14:00:56 +0800

    fix: 調整 Cucumber 測試配置的格式

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -9,6 +9,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber-reports/cucumber.html")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value = "html:target/cucumber-reports/cucumber.html")
 public class RunCucumberTest {
 }
